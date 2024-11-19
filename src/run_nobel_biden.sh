b71a55c2b1650d974775021979e537f102000feb
wangjiangshan0725 <3219632827@qq.com>
2024-11-19 19:44:21 +0800

    improve readme

--- src/run_nobel_biden.sh
+++ src/run_nobel_biden.sh
@@ -1,7 +1,7 @@
 python edit.py  --source_prompt "" \
                 --target_prompt "A minimalistic line-drawing portrait of Joe Biden with black lines and light brown shadow" \
                 --guidance 2.5 \
-                --source_img_dir '/examples/source/nobel.jpg' \
+                --source_img_dir 'examples/source/nobel.jpg' \
                 --num_steps 25  \
                 --inject 2 \
                 --name 'flux-dev'  \
--- src/run_nobel_trump.sh
+++ src/run_nobel_trump.sh
@@ -1,7 +1,7 @@
 python edit.py  --source_prompt "" \
                 --target_prompt "A minimalistic line-drawing portrait of Donald Trump with black lines and brown shadow" \
                 --guidance 2.5 \
-                --source_img_dir '/examples/source/nobel.jpg' \
+                --source_img_dir 'examples/source/nobel.jpg' \
                 --num_steps 25  \
                 --inject 3 \
                 --name 'flux-dev'  \
